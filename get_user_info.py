698e44f3dc403c289b2b810f593b96225665ecc2
DouglasXie <douglas2011@qq.com>
2018-08-02 14:41:34 +0800

    add tuling for chatting

--- get_user_info.py
+++ get_user_info.py
@@ -1,23 +1,55 @@
 #coding=utf-8
-#author:微信公众号：大数据前沿
-#查看代码讲解，视频教程，请微信添加好友搜索公众号[大数据前沿]查看历史消息获取。
+#author:Douglas Xie
 
 import itchat
 import json
 import requests
 import codecs
 
-sex_dict = {}
-sex_dict['0'] = "其他"
-sex_dict['1'] = "男"
-sex_dict['2'] = "女"
+from pandas import DataFrame
 
-message_dict = {
-    "二胖":"更多好玩的内容请关注公众号：大数据前沿（id:bigdataqianyan）",
-    "你好":"你好啊，这条消息是自动回复的。",
-    "备忘录":"早上10.30参加产品发布会\n今晚隔壁王总找你开会"
-}
-#下载好友头像
+
+sex_dict = {'0': "其他",
+            '1': "男",
+            '2': "女"}
+
+
+tuling_white_list = []
+ask_list = []
+
+# 图灵机器人API
+def tuling(info):
+    appkey = "338e06fdb0ec4a2283938aa997b2828d"
+    url = "http://www.tuling123.com/openapi/api?key=%s&info=%s"%(appkey,info)
+    req = requests.get(url)
+    content = req.text
+    data = json.loads(content)
+    answer = data['text']
+    return answer
+
+# 更新图灵机器人白名单.
+def update_tuling_white_list(frined_list):
+    global tuling_white_list
+    json_file_tuling_white_list = './data/tuling_white_list.json'
+
+    simple_friend_list = []
+    for friend in friends_list:
+        simple_friend_list.append(friend['UserName'])
+
+    try:
+        with codecs.open(json_file_tuling_white_list, encoding='utf-8') as f:
+            tuling_white_list = json.load(f)
+
+        for person in tuling_white_list:
+            if person['UserName'] not in simple_friend_list:
+                tuling_white_list.remove(person)
+    except:
+        tuling_white_list = [friends_list[0]['UserName']]
+
+    with codecs.open(json_file_tuling_white_list, 'w', encoding='utf-8') as f:
+        f.write(json.dumps(tuling_white_list, ensure_ascii=False))
+
+# 下载好友头像
 def download_images(frined_list):
     image_dir = "./images/"
     num = 1
@@ -28,29 +60,48 @@ def download_images(frined_list):
         with open(image_dir+image_name, 'wb') as file:
             file.write(img)
 
+# 保存好友信息
 def save_data(frined_list):
     out_file_name = "./data/friends.json"
     with codecs.open(out_file_name, 'w', encoding='utf-8') as json_file:
         json_file.write(json.dumps(frined_list,ensure_ascii=False))
 
-@itchat.msg_register(itchat.content.TEXT)
-def print_content(msg):
-    NickName = msg['User']['NickName']
-    user = itchat.search_friends(name=NickName)[0]
-    text = msg['Text']
+    frame = DataFrame(frined_list)
+    frame.to_excel("./data/friends.xlsx", index=True)
 
-    if text in message_dict.keys():
-        user.send(message_dict[text])
-    else:
-        user.send(u"你好啊%s,我目前还不支持这个功能"%NickName)
+# 文字消息自动回复
+@itchat.msg_register([itchat.content.TEXT, itchat.content.MAP, itchat.content.CARD, itchat.content.NOTE, itchat.content.SHARING])
+def text_reply(msg):
+    print("msg_in: %s: %s" % (msg['FromUserName'], msg['Text']))
+    if msg['FromUserName'] in tuling_white_list:
+        itchat.send('AI: %s' % tuling(msg['Text']), msg['FromUserName'])
+
+# 图片，视频文件自动回复
+@itchat.msg_register([itchat.content.PICTURE, itchat.content.RECORDING, itchat.content.ATTACHMENT, itchat.content.VIDEO])
+def download_files(msg):
+    msg.download("./download/" + msg.fileName)
+    typeSymbol = {
+        itchat.content.PICTURE: 'img',
+        itchat.content.VIDEO: 'vid', }.get(msg.type, 'fil')
+    return '@%s@%s' % (typeSymbol, msg.fileName)
+
+# 群消息自动回复
+@itchat.msg_register(itchat.content.TEXT, isGroupChat=True)
+def group_text_reply(msg):
+    if msg.isAt:
+        print("group_msg_in: %s at me: %s" % (msg['actualNickName'], msg['Text']))
+        print("msg raw: ", msg)
+        # msg.user.send(u'@%s\u2005 I received: %s' % (
+        #     msg.actualNickName, msg.text))
 
 
 if __name__ == '__main__':
-    itchat.auto_login()
-    
-    friends = itchat.get_friends(update=True)[0:]#获取好友信息
-    friends_list = []
 
+    itchat.auto_login(hotReload=True)    
+
+    #获取好友信息
+    friends = itchat.get_friends(update=True)[0:]
+    friends_list = []
     for friend in friends:
         item = {}
         item['NickName'] = friend['NickName']
@@ -61,12 +112,12 @@ if __name__ == '__main__':
         item['UserName'] = friend['UserName']
 
         friends_list.append(item)
-        #print(item)
+        print(item)
 
+    update_tuling_white_list(friends_list)
     save_data(friends_list)
-    download_images(friends_list)
+    # download_images(friends_list)
 
-    
-    user = itchat.search_friends(name=u'二胖')[0]
-    user.send(u'hello,这是一条来自机器人的消息')
+    user = itchat.search_friends(name=u'Douglas.XIE')[0]
+    user.send(u'Hello 主人,聊天机器人已经启动')
     itchat.run()
